98693220c1121e5eacf00b6e181f91c3ef260ded
FHorschig <f.horschig@googlemail.com>
2014-03-24 12:21:01 +0100

    Created Webapp plus first controller and index.html

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '518f4989bb40e0a138f7f5ef9685d50bc5307c4ec628ae6f980327691acf8571cb9a75f5cd712f1b0c6cc048cd5d56c5e6fe79544d212fb688416d97a42255e1'
